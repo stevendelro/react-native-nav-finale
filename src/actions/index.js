6a0d351c5a720c2319e9b1223335031d276985b3
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-09 16:27:48 -0700

    Created passwordChanged action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
-import { EMAIL_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
 
-export const emailChanged = text => {
+export const emailChanged = email => {
   return {
     type: EMAIL_CHANGED,
-    payload: text
+    payload: email
+  };
+};
+
+export const passwordChanged = password => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: password
   };
 };
