49ca98233e1de5a369d0757f8b5f69d2ac11e1dd
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-09 12:07:03 -0700

    Installed, configured and initialized firebase.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBT9prxL3REqJAPtkoQ0B45Hth0fuKe14k',
+      authDomain: 'react-native-nav-finale.firebaseapp.com',
+      databaseURL: 'https://react-native-nav-finale.firebaseio.com',
+      projectId: 'react-native-nav-finale',
+      storageBucket: 'react-native-nav-finale.appspot.com',
+      messagingSenderId: '128332415434'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
