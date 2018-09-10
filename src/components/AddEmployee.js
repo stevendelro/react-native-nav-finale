f36f38eef1338cc81d8cf4ae81698e940956393f
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-10 11:54:10 -0700

    Set inputs to fire off EditEmployee with correct properties and values.

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { EditEmployee } from '../actions';
 import { Card, CardSection, Input, Button } from '../common';
 
 class AddEmployee extends Component {
   render() {
+    const { name, phone } = this.props;
     return (
       <Card>
         <CardSection>
-          <Input label="Name" placeholder="Jane" />
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={name}
+            onChange={employeeName =>
+              this.props.EditEmployee({
+                prop: 'name',
+                value: employeeName
+              })
+            }
+          />
         </CardSection>
         <CardSection>
-          <Input label="Phone" placeholder="012-435-6789" />
+          <Input
+            label="Phone"
+            placeholder="012-345-6789"
+            value={phone}
+            onChange={phoneNumber =>
+              this.props.EditEmployee({
+                prop: 'phone',
+                value: phoneNumber
+              })
+            }
+          />
         </CardSection>
         <CardSection>
           <Button>Add</Button>
@@ -19,4 +42,12 @@ class AddEmployee extends Component {
   }
 }
 
-export default AddEmployee;
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.form;
+  return { name, phone, shift };
+};
+
+export default connect(
+  mapStateToProps,
+  { EditEmployee }
+)(AddEmployee);
