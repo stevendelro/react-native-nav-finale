db9119119ce3ad03ed2c8cbc888861f4487e0880
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-10 11:30:35 -0700

    FormReducer set up to handle EDIT_EMPLOYEE.

--- src/reducers/FormReducer.js
+++ src/reducers/FormReducer.js
@@ -1,11 +1,21 @@
 import { EDIT_EMPLOYEE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
+  const { prop, value } = payload;
 
   switch (type) {
+    case EDIT_EMPLOYEE:
+      return {
+        ...state,
+        [prop]: value
+      };
     default:
       return state;
   }
