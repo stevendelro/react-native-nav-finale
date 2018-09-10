09b38f4022c556a606cdfa0f9bcca14bfc19d06b
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-09 17:48:28 -0700

    AuthReducer set to receive successful login payload

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,17 +1,27 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
+} from '../actions/types';
 
-const INITIAL_STATE = { email: '', password: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  loading: false,
+  error: '',
+  user: null
+};
 
 export default (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
-
+  console.log('INSIDE REDUCER: ', action);
   switch (type) {
     case EMAIL_CHANGED:
       return { ...state, email: payload };
-    
     case PASSWORD_CHANGED:
       return { ...state, password: payload };
-
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: payload };
     default:
       return state;
   }
