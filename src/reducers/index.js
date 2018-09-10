e1c1c9c038ec59638fdba52e8d035bc6444ab9ec
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-10 11:52:33 -0700

    Added FormReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import FormReducer from './FormReducer';
 
 export default combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  form: FormReducer
 });
