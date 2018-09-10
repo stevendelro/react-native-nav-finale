56e8df720ee7cf16a9eb8aba1fd2aaeda9ac1ce9
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-10 11:08:25 -0700

    Removed dummy text. Created a Card with a few inputs and a button.

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from '../common';
 
 class AddEmployee extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input label="Name" placeholder="Jane" />
+        </CardSection>
+        <CardSection>
+          <Input label="Phone" placeholder="012-435-6789" />
+        </CardSection>
+        <CardSection>
+          <Button>Add</Button>
+        </CardSection>
+      </Card>
     );
   }
 }
