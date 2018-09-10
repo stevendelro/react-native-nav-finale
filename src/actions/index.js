1bf6100c736cd2848034fc46ed1fc3f4690b17fa
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-09 18:09:23 -0700

    Cleaned up loginUser

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAILED
+} from './types';
 
 export const emailChanged = email => {
   return {
@@ -20,22 +25,27 @@ export const loginUser = ({ email, password }) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({
-          type: LOGIN_USER_SUCCESS,
-          payload: user
-        });
-      })
+      .then(user => loginUserSuccess(dispatch, user))
       .catch(() => {
         firebase
           .auth()
           .createUserWithEmailAndPassword(email, password)
-          .then(user => {
-            dispatch({
-              type: LOGIN_USER_SUCCESS,
-              payload: user
-            });
-          });
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(error => loginUserFailed(dispatch, error));
       });
   };
+};
+
+const loginUserFailed = (dispatch, error) => {
+  dispatch({ 
+    type: LOGIN_USER_FAILED,
+    payload: error
+  });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
 };
