031a585d2d083f34ff32fe43cd3c8876f3511dca
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-09 17:45:37 -0700

    Used thunk within loginUser to dispatch LOGIN_USER_SUCCESS

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 export const emailChanged = email => {
   return {
@@ -11,5 +12,20 @@ export const passwordChanged = password => {
   return {
     type: PASSWORD_CHANGED,
     payload: password
+  };
+};
+
+export const loginUser = ({ email, password }) => {
+  return dispatch => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({
+          type: LOGIN_USER_SUCCESS,
+          payload: user
+        });
+      })
+      .catch(() => {})
   };
 };
