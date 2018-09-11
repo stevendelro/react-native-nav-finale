3db1738feefe3e1ef53619dc99b4c34551add82f
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-11 11:56:12 -0700

    Directing user to AddEmployee scene with the selected employee's info

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from '../common';
 
 class ListItem extends Component {
+  onRowPress() {
+    Actions.AddEmployee({ employee: this.props.employee });
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>{name}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>{name}</Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
