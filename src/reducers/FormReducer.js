97a1234ef62b9cb3c3bf4732d7d0fe1a9d336b4e
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-10 17:41:43 -0700

    Wipe form data clean after updating firebase.

--- src/reducers/FormReducer.js
+++ src/reducers/FormReducer.js
@@ -1,4 +1,4 @@
-import { EDIT_EMPLOYEE } from '../actions/types';
+import { EDIT_EMPLOYEE, ADD_NEW_EMPLOYEE } from '../actions/types';
 
 const INITIAL_STATE = {
   name: '',
@@ -10,6 +10,8 @@ export default (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
+    case ADD_NEW_EMPLOYEE:
+      return INITIAL_STATE;
     case EDIT_EMPLOYEE:
       return {
         ...state,
