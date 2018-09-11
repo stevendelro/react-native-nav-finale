ac3058763cdd6dcb3abf8f4da4c098a85d5c8041
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-11 12:41:07 -0700

    On WillMount, take the selected employee data and update the reducer.

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,22 +1,40 @@
+import each from 'lodash/each';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import { editEmployee } from '../actions';
 import { Card, CardSection, Button } from '../common';
 
 class EditEmployee extends Component {
+  componentWillMount() {
+    each(this.props.employee, (value, prop) => {
+      this.props.editEmployee({ prop, value });
+    });
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    console.log('EDITEMPLOYEE:', name, phone, shift);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>Save Changes</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
         </CardSection>
       </Card>
     );
   }
 }
 
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.form;
+  return { name, phone, shift };
+};
+
 export default connect(
-  null,
-  {}
+  mapStateToProps,
+  { editEmployee }
 )(EditEmployee);
