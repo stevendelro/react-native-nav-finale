b2bcb07e5ea2ff6880dd273452590aa0a7d43654
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-10 17:38:59 -0700

    Push input to firebase, wipe state clean, bring user back to list

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,24 @@
-import { EDIT_EMPLOYEE } from './types';
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import { EDIT_EMPLOYEE, ADD_NEW_EMPLOYEE } from './types';
 
-export const EditEmployee = ({ prop, value }) => {
+export const editEmployee = ({ prop, value }) => {
   return {
     type: EDIT_EMPLOYEE,
     payload: { prop, value }
+  };
+};
+
+export const addNewEmployee = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: ADD_NEW_EMPLOYEE });
+        Actions.employeeList();
+      });
   };
 };
